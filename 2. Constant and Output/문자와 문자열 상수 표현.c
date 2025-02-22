f8bd3abc2fc33166227f4692e244932bf9665d88
kite0510 <wjddus1994@naver.com>
2025-02-22 23:22:55 +0900

    Create 문자와 문자열 상수 표현.c

---
+++ 2. Constant and Output/문자와 문자열 상수 표현.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%c\n", 'A');  //문자 상수 출력
+	printf("%s\n", "A");  //문자열 상수 출력
+	printf("%c은 %s입니다.", '1', "first"); //문자와 문자열 함께 출력
+
+	return 0;
+}
