e9832cbf5d3eee9a3acd06181e1a0bead4ac4cee
kite0510 <wjddus1994@naver.com>
2025-04-26 21:22:53 +0900

    Create 2.파일의 내용을 화면에 출력하기.c

---
+++ 18. 파일 입출력/2.파일의 내용을 화면에 출력하기.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+    FILE *fp;  // 파일 포인터 선언
+    int ch;    // 입력한 문자를 저장할 변수
+
+    fp = fopen("a.txt", "r"); // 읽기 전용으로 파일 개방
+    if (fp == NULL) {        // 파일이 개방되었는지 확인
+        printf("파일이 열리지 않았습니다.\n");
+        return 1;
+    }
+
+    while (1) { // 파일의 내용을 모두 읽을 때까지 반복
+        ch = fgetc(fp); // 개방한 파일에서 문자 입력
+        if (ch == EOF) { // 함수의 반환값이 EOF면 종료 (파일 끝 또는 오류)
+            break;
+        }
+        putchar(ch); // 입력한 문자를 화면에 출력
+    }
+
+    fclose(fp); // 파일 닫음
+
+    return 0;
+}
